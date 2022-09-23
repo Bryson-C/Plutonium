866715ccb3ef86a8904e10ae0a9096b79cd26f24
WhisperKnight <61612853+Bryson-C@users.noreply.github.com>
2022-09-22 22:49:23 -0600

    Add files via upload

--- Shaders/f.glsl
+++ Shaders/f.glsl
@@ -1,9 +1,16 @@
 #version 450
 
 layout(location = 0) in vec4 inColor;
+layout(location = 1) in vec2 inTexCoords;
+layout(location = 2) in uint inTexId;
 
 layout(location = 0) out vec4 outColor;
 
+layout(set = 1, binding = 0) uniform sampler textureSampler;
+layout(set = 2, binding = 0) uniform texture2D textures[8];
+
+
 void main() {
-    outColor = inColor;
+    outColor = texture( sampler2D(textures[inTexId], textureSampler), inTexCoords );
+    //outColor = inColor;
 }
--- Shaders/v.glsl
+++ Shaders/v.glsl
@@ -2,11 +2,26 @@
 
 layout(location = 0) in vec3 inPosition;
 layout(location = 1) in vec3 inColor;
+layout(location = 2) in vec2 inTexCoords;
+layout(location = 3) in uint textureId;
+
+
+layout(set = 0, binding = 0) uniform UBO {
+    float x;
+    float y;
+} UniformBufferObject;
 
 layout(location = 0) out vec4 outColor;
+layout(location = 1) out vec2 outTexCoords;
+layout(location = 2) out float outTextureId;
 
 
 void main() {
-    gl_Position = vec4(inPosition, 1.0);
+
+    vec3 position = vec3(inPosition.x + UniformBufferObject.x, inPosition.y + UniformBufferObject.y, inPosition.z);
+
+    gl_Position = vec4(position, 1.0);
     outColor = vec4(inColor, 1.0);
+    outTexCoords = inTexCoords;
+    outTextureId = textureId;
 }
